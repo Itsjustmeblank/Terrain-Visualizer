7e4df7440ece75b808b5ff7743636c48a5c29e50
Jake Ramey <jake.ramey@utdallas.edu>
2025-12-05 16:47:01 -0600

    Consistent issue with line 86 error. Recieving print statements

--- terrain_visualizer.py
+++ terrain_visualizer.py
@@ -13,20 +13,23 @@ class TerrainVisualizer:
 
     def load_mesh(self):
         sel = om.MGlobal.getActiveSelectionList()
+        print(f"[LOAD MESH] Selection count: {sel.length()}")
 
-        if sel.length() == 0:                               #safegaurd 
+        if sel.length() == 0:                       #safegaurd 
             om.MGlobal.displayError("Please select a mesh.")
             return False
 
-        dag = sel.getDagPath(0)                 
+        dag = sel.getDagPath(0)             
         dag.extendToShape()
+        print(f"[LOAD MESH] Mesh DAG path: {dag.fullPathName()}")
 
         self.mesh_dag = dag
         self.mesh_fn = om.MFnMesh(dag)
+        print(f"[LOAD MESH] MFnMesh created for: {dag.fullPathName()}")
 
         return True
 
-    def get_palette(self):              #actual RGBs
+    def get_palette(self):           #actual RGBs
         palettes = {
             "hot":  [(0, (0.3, 0.1, 0.0)), (0.5, (1.0, 0.4, 0.0)), (1.0, (1.0, 1.0, 0.7))], #brown, red, yellow/tan
             "mild": [(0, (0.0, 0.3, 0.0)), (0.5, (0.2, 0.8, 0.2)), (1.0, (0.9, 0.9, 0.9))],
@@ -58,41 +61,67 @@ class TerrainVisualizer:
 
 
         last_color = palette[-1][1]
-        return om.MColor([last_color[0], last_color[1], last_color[2], 1])      #if too high will just color with last rgb in the class
+
+        """return [last_color[0], last_color[1], last_color[2], 1]"""      #if too high will just color with last rgb in the class
+        return om.MColor((last_color[0], last_color[1], last_color[2], 1.0))
+    def apply(self):
+        if not self.load_mesh():
+            return
+
+        mesh = self.mesh_fn
+        verts = mesh.getPoints(om.MSpace.kWorld)
+
+    
+        heights = [v.y for v in verts]
+        min_h = min(heights)
+        max_h = max(heights)
+        height_range = max_h - min_h + 1e-6  # Prevent division by zero
+
+        
+        colors = [self.height_to_color((v.y - min_h) / height_range) for v in verts]
 
 
-def apply(self):
-    if not self.load_mesh():
-        return
+        if self.color_set_name not in mesh.getColorSetNames():           #create colorset
+            mesh.createColorSet(self.color_set_name)
 
-    mesh = self.mesh_fn
-    verts = mesh.getPoints(om.MSpace.kWorld)
+        mesh.setCurrentColorSetName(self.color_set_name)
 
- 
-    heights = [v.y for v in verts]
-    min_h = min(heights)
-    max_h = max(heights)
-    height_range = max_h - min_h + 1e-6  # Prevent division by zero
-
-   
-    colors = [self.height_to_color((v.y - min_h) / height_range) for v in verts]
+        mesh.setColors(colors, self.color_set_name)
 
 
-    if self.color_set_name not in mesh.getColorSetNames():          #create colorset
-        mesh.createColorSet(self.color_set_name)
-
-    mesh.setCurrentColorSetName(self.color_set_name)
-
-
-    mesh.setColors(colors, self.color_set_name)                     #apply color to colorset
-    mesh.assignColors(list(range(len(colors))), self.color_set_name)
-    mesh.updateSurface()
+        
+        
+        mesh.setColors(colors, self.color_set_name)  # colors must match vertex count
+        vertex_indices = list(range(len(colors)))
+        mesh.assignColors(vertex_indices, colorSetName=self.color_set_name)
 
 
-    mesh_name = self.mesh_dag.fullPathName()                #lambert?
-    shader = cmds.shadingNode("lambert", asShader=True, name="terrainShader")
-    sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="terrainSG")
-    cmds.connectAttr(shader + ".outColor", sg + ".surfaceShader")
-    cmds.sets(mesh_name, e=True, forceElement=sg)
 
-    om.MGlobal.displayInfo(f"TerrainVisualizer: Coloring applied using '{self.climate}' climate.")
+        mesh.updateSurface()
+
+#mesh.assignColors(vertex_colors, self.color_set_name) #(list(range(len(colors))), self.color_set_name)"""
+
+        mesh_name = self.mesh_dag.fullPathName()                     #lambert?
+        shader = cmds.shadingNode("lambert", asShader=True, name="terrainShader")
+        sg = cmds.sets(renderable=True, noSurfaceShader=True, empty=True, name="terrainSG")
+        cmds.connectAttr(shader + ".outColor", sg + ".surfaceShader")
+        cmds.sets(mesh_name, e=True, forceElement=sg)
+
+        om.MGlobal.displayInfo(f"TerrainVisualizer: Coloring applied using '{self.climate}' climate.")
+
+
+"""
+import sys
+import importlib
+
+module_folder = r"C:\Users\jaker\OneDrive\Desktop\Programming\PFDA\Terrain_Visualizer\Terrain-Visualizer"
+if module_folder not in sys.path:
+    sys.path.append(module_folder)
+
+
+import terrain_visualizer
+importlib.reload(terrain_visualizer)
+
+tv = terrain_visualizer.TerrainVisualizer(climate="mild")
+tv.apply()
+"""
